b22262d4ea99b23dbf70125d45a364a0a8b7ba2f
StrangeFlame <25YMUHA524I@student.ri.edu.sg>
2023-05-23 22:39:12 +0800

    23/5 Basic backspace key utilising html and css (might port to js entirely)

--- sketch.js
+++ sketch.js
@@ -1,3 +1,21 @@
+function animateBackspace() {
+  const backspace = document.getElementById("backspace");
+  backspace.classList.add("pressed");
+}
+
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Backspace") {
+    animateBackspace();
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Backspace") {
+    const backspace = document.getElementById("backspace");
+    backspace.classList.remove("pressed");
+  }
+});
+
 function setup() {
   createCanvas(400, 400);
 }
